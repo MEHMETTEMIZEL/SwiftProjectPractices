52942056ae00c06f113e539f3713fe8828515fcc
MEHMETTEMIZEL <meehmettemizel80@gmail.com>
2023-10-23 07:25:40 +0300

    I Am Poor project is added.

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Mehmet Temizel / Kuveyt Türk - BT-Dijital Bankacılık 1 on 8.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
